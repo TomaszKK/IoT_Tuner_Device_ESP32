2c93416ac50c276190a3fe11313a682df76b2eb3
TomaszKK <242136@edu.p.lodz.pl>
2024-09-03 14:23:14 +0200

    Done version 1.0

---
+++ BLE_notify/yinacf.h
@@ -0,0 +1,29 @@
+#ifndef Yin_h
+#define Yin_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+#define YIN_SAMPLING_RATE 44100
+#define YIN_DEFAULT_THRESHOLD 0.15
+
+typedef struct _Yin {
+    int16_t bufferSize;
+    int16_t halfBufferSize;
+    float* yinBuffer;
+    float probability;
+    float threshold;
+} Yin;
+
+void Yin_init(Yin *yin, int16_t bufferSize, float threshold);
+float Yin_getPitch(Yin *yin, int16_t* buffer);
+float Yin_getProbability(Yin *yin);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
