73fb17b66e3641ef9b590d54e82e4e05c1e82de3
TomaszKK <242136@edu.p.lodz.pl>
2024-10-13 12:36:50 +0200

    BLE change to nimBLE, which works - needs changes to fit to the mobile app. (name, id, etc) - generally works

--- tuner-v.1.2/tuner_betterFreq/tuner_betterFreq.ino
+++ tuner-v.1.2/tuner_betterFreq/tuner_betterFreq.ino
@@ -1,7 +1,3 @@
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
 #include "yinacf.h"
 #include "driver/gpio.h"
 #include "driver/adc.h"
@@ -10,14 +6,14 @@
 #include "esp_adc/adc_oneshot.h"
 #include "esp_adc/adc_cali.h"
 #include "esp_adc/adc_cali_scheme.h"
+#include <NimBLEDevice.h>
 
-// Define constants and variables for ADC and BLE
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 #define DEVICE_NAME         "TUNER IOT"
-#define PIN 32              // Pin where the mic is connected
-#define BUFFER_SIZE 1024   // Buffer size for ADC samples
-#define PITCH_BUFFER_SIZE 5
+#define PIN 32              
+#define BUFFER_SIZE 1024 * 2
+#define PITCH_BUFFER_SIZE 2
 
 #define EXAMPLE_ADC_ATTEN           ADC_ATTEN_DB_12
 
@@ -25,26 +21,20 @@ static adc1_channel_t channel = ADC1_CHANNEL_4;
 static const adc_bits_width_t width = ADC_WIDTH_BIT_12;
 static const adc_atten_t atten = ADC_ATTEN_DB_12;
 static const adc_unit_t unit = ADC_UNIT_1;
+adc_oneshot_unit_handle_t adc1_handle;
 
 Yin yin;
 int16_t *buffer;
 bool isBufferFull = false;
 int nextBufferItem = 0;
-
-BLEServer* pServer = NULL;
-BLECharacteristic* pCharacteristic = NULL;
-bool deviceConnected = false;
-bool oldDeviceConnected = false;
-
 float pitchBuffer[PITCH_BUFFER_SIZE] = {0};  // Initialize with zeros
 int bufferIndex = 0;
-float alpha = 0.2;  // Smoothing factor, adjust between 0.0 (very smooth) and 1.0 (no smoothing)
+float alpha = 0.1;  // Smoothing factor, adjust between 0.0 (very smooth) and 1.0 (no smoothing)
 float filteredValue = 0;
 
-adc_oneshot_unit_handle_t adc1_handle;
-static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
-static void example_adc_calibration_deinit(adc_cali_handle_t handle);
-
+static NimBLEServer* pServer;
+static BLEDescriptorCallbacks dscCallbacks;
+static BLECharacteristicCallbacks chrCallbacks;
 
 static void check_efuse(void)
 {
@@ -72,10 +62,45 @@ static void check_efuse(void)
 #endif
 }
 
+float exponentialMovingAverageFilter(int newValue) {
+    filteredValue = alpha * newValue + (1 - alpha) * filteredValue;
+    return filteredValue;
+}
+
+float calculateAveragePitch() {
+    float sum = 0.0;
+    int count = 0;
+
+    // Iterate through the buffer and find values that occur multiple times within ±1 Hz
+    for (int i = 0; i < PITCH_BUFFER_SIZE; i++) {
+        if (pitchBuffer[i] > 0) {  // Exclude zeros
+            int occurrenceCount = 0;
+            float currentPitch = pitchBuffer[i];
+
+            // Check how many times the current pitch occurs within ±1 Hz in the buffer
+            for (int j = 0; j < PITCH_BUFFER_SIZE; j++) {
+                if (j != i && fabs(pitchBuffer[j] - currentPitch) <= 10.0) {
+                    occurrenceCount++;
+                }
+            }
+
+            // If the pitch occurs more than once, add it to the sum
+            if (occurrenceCount > 0) {  // Meaning it has at least one "close match"
+                sum += currentPitch;
+                count++;
+            }
+        }
+    }
+
+    // Return the average of the filtered values, or 0 if no valid values were found
+    return (count > 0) ? (sum / count) : 0;
+}
+
 void readData() {
     while (nextBufferItem < BUFFER_SIZE) {
-        buffer[nextBufferItem++] = (int16_t)adc1_get_raw((adc1_channel_t)channel);
-        delayMicroseconds(1);
+        buffer[nextBufferItem++] = exponentialMovingAverageFilter((int16_t)adc1_get_raw((adc1_channel_t)channel));
+        // buffer[nextBufferItem++] = (int16_t)adc1_get_raw((adc1_channel_t)channel);
+        delayMicroseconds(2);
     }
 
     if (nextBufferItem >= BUFFER_SIZE) {
@@ -83,9 +108,125 @@ void readData() {
     }    
 }
 
+class ServerCallbacks: public NimBLEServerCallbacks {
+    void onConnect(NimBLEServer* pServer) {
+        Serial.println("Client connected");
+        // Serial.println("Multi-connect support: start advertising");
+        // NimBLEDevice::startAdvertising();
+    };
+
+    void onConnect(NimBLEServer* pServer, ble_gap_conn_desc* desc) {
+        Serial.print("Client address: ");
+        Serial.println(NimBLEAddress(desc->peer_ota_addr).toString().c_str());
+        /** We can use the connection handle here to ask for different connection parameters.
+         *  Args: connection handle, min connection interval, max connection interval
+         *  latency, supervision timeout.
+         *  Units; Min/Max Intervals: 1.25 millisecond increments.
+         *  Latency: number of intervals allowed to skip.
+         *  Timeout: 10 millisecond increments, try for 5x interval time for best results.
+         */
+        pServer->updateConnParams(desc->conn_handle, 6, 6, 0, 10);
+    };
+
+    void onDisconnect(NimBLEServer* pServer) {
+        Serial.println("Client disconnected - start advertising");
+        NimBLEDevice::startAdvertising();
+    };
+
+    void onMTUChange(uint16_t MTU, ble_gap_conn_desc* desc) {
+        Serial.printf("MTU updated: %u for connection ID: %u\n", MTU, desc->conn_handle);
+    };
+
+    uint32_t onPassKeyRequest(){
+        Serial.println("Server Passkey Request");
+        /** This should return a random 6 digit number for security
+         *  or make your own static passkey as done here.
+         */
+        return 123456;
+    };
+
+    bool onConfirmPIN(uint32_t pass_key){
+        Serial.print("The passkey YES/NO number: ");Serial.println(pass_key);
+        /** Return false if passkeys don't match. */
+        return true;
+    };
+
+    void onAuthenticationComplete(ble_gap_conn_desc* desc){
+        /** Check that encryption was successful, if not we disconnect the client */
+        if(!desc->sec_state.encrypted) {
+            NimBLEDevice::getServer()->disconnect(desc->conn_handle);
+            Serial.println("Encrypt connection failed - disconnecting client");
+            return;
+        }
+        Serial.println("Starting BLE work!");
+    };
+};
+
+class CharacteristicCallbacks: public NimBLECharacteristicCallbacks {
+    void onRead(NimBLECharacteristic* pCharacteristic){
+        Serial.print(pCharacteristic->getUUID().toString().c_str());
+        Serial.print(": onRead(), value: ");
+        Serial.println(pCharacteristic->getValue().c_str());
+    };
+
+    void onWrite(NimBLECharacteristic* pCharacteristic) {
+        Serial.print(pCharacteristic->getUUID().toString().c_str());
+        Serial.print(": onWrite(), value: ");
+        Serial.println(pCharacteristic->getValue().c_str());
+    };
+
+    void onNotify(NimBLECharacteristic* pCharacteristic) {
+        // Serial.println("Sending notification to clients");
+    };
+
+    void onStatus(NimBLECharacteristic* pCharacteristic, Status status, int code) {
+        String str = ("Notification/Indication status code: ");
+        str += status;
+        str += ", return code: ";
+        str += code;
+        str += ", ";
+        str += NimBLEUtils::returnCodeToString(code);
+        // Serial.println(str);
+    };
+
+    void onSubscribe(NimBLECharacteristic* pCharacteristic, ble_gap_conn_desc* desc, uint16_t subValue) {
+        String str = "Client ID: ";
+        str += desc->conn_handle;
+        str += " Address: ";
+        str += std::string(NimBLEAddress(desc->peer_ota_addr)).c_str();
+        if(subValue == 0) {
+            str += " Unsubscribed to ";
+        }else if(subValue == 1) {
+            str += " Subscribed to notfications for ";
+        } else if(subValue == 2) {
+            str += " Subscribed to indications for ";
+        } else if(subValue == 3) {
+            str += " Subscribed to notifications and indications for ";
+        }
+        str += std::string(pCharacteristic->getUUID()).c_str();
+
+        // Serial.println(str);
+    };
+};
+
+class DescriptorCallbacks : public NimBLEDescriptorCallbacks {
+    void onWrite(NimBLEDescriptor* pDescriptor) {
+        std::string dscVal = pDescriptor->getValue();
+        Serial.print("Descriptor witten value:");
+        Serial.println(dscVal.c_str());
+    };
+
+    void onRead(NimBLEDescriptor* pDescriptor) {
+        Serial.print(pDescriptor->getUUID().toString().c_str());
+        Serial.println(" Descriptor read");
+    };
+};
+
+
 void setup() {
     Serial.begin(115200);
-
+    Serial.println("Starting NimBLE Server");
+    
     check_efuse();
     adc1_config_width(width);
     adc1_config_channel_atten(channel, atten);
@@ -97,95 +238,107 @@ void setup() {
     }
 
     // Initialize YIN pitch detection algorithm
-    Yin_init(&yin, BUFFER_SIZE, 0.10);
+    Yin_init(&yin, BUFFER_SIZE, YIN_DEFAULT_THRESHOLD);
+    
+    /** sets device name */
+    NimBLEDevice::init("NimBLE-Arduino");
+
+    /** Optional: set the transmit power, default is 3db */
+    NimBLEDevice::setPower(ESP_PWR_LVL_N0); /** +9db */
 
 
+    /** Set the IO capabilities of the device, each option will trigger a different pairing method.
+     *  BLE_HS_IO_DISPLAY_ONLY    - Passkey pairing
+     *  BLE_HS_IO_DISPLAY_YESNO   - Numeric comparison pairing
+     *  BLE_HS_IO_NO_INPUT_OUTPUT - DEFAULT setting - just works pairing
+     */
+    //NimBLEDevice::setSecurityIOCap(BLE_HS_IO_DISPLAY_ONLY); // use passkey
+    //NimBLEDevice::setSecurityIOCap(BLE_HS_IO_DISPLAY_YESNO); //use numeric comparison
+
+    /** 2 different ways to set security - both calls achieve the same result.
+     *  no bonding, no man in the middle protection, secure connections.
+     *
+     *  These are the default values, only shown here for demonstration.
+     */
+    //NimBLEDevice::setSecurityAuth(false, false, true);
+    NimBLEDevice::setSecurityAuth(/*BLE_SM_PAIR_AUTHREQ_BOND | BLE_SM_PAIR_AUTHREQ_MITM |*/ BLE_SM_PAIR_AUTHREQ_SC);
+
+    pServer = NimBLEDevice::createServer();
+    pServer->setCallbacks(new ServerCallbacks());
+
+    NimBLEService* pBaadService = pServer->createService("BAAD");
+    NimBLECharacteristic* pFoodCharacteristic = pBaadService->createCharacteristic(
+                                               "F00D",
+                                               NIMBLE_PROPERTY::NOTIFY
+                                              );
+
+    pFoodCharacteristic->setValue("Fries");
+    pFoodCharacteristic->setCallbacks(&chrCallbacks);
+
+    // NimBLEDescriptor* pC01Ddsc = pFoodCharacteristic->createDescriptor(
+    //                                            "C01D",
+    //                                            NIMBLE_PROPERTY::READ |
+    //                                            NIMBLE_PROPERTY::WRITE|
+    //                                            NIMBLE_PROPERTY::WRITE_ENC, // only allow writing if paired / encrypted
+    //                                            20
+    //                                           );
+    // pC01Ddsc->setValue("Send it back!");
+    // pC01Ddsc->setCallbacks(&dscCallbacks);
+
+    pBaadService->start();
+
+    NimBLEAdvertising* pAdvertising = NimBLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(pBaadService->getUUID());
+    pAdvertising->setMaxInterval(100);
+    pAdvertising->setMinInterval(100);
+    // pAdvertising->addTxPower();
+
+    pAdvertising->setScanResponse(false);
+    pAdvertising->start();
+
+    Serial.println("Advertising Started");
 }
-
 
 
 void loop() {
+  if(pServer->getConnectedCount()) {
+    readData(); 
+    if (isBufferFull) {
+      nextBufferItem = 0;
+      isBufferFull = false;
 
-        readData();
+      float pitch = Yin_getPitch(&yin, buffer);
+      pitch = pitch / 2; 
+      // Serial.printf("Pitch = %.1f Hz\n", pitch); 
+          
+      if(pitch == -0.5){
+        pitch = 0;
+      }
 
-        if (isBufferFull) {
-       
-            nextBufferItem = 0;
-            isBufferFull = false;
+      pitchBuffer[bufferIndex] = pitch;
+      bufferIndex++;
 
-            float pitch = Yin_getPitch(&yin, buffer);
-            pitch = pitch / 2; 
-            Serial.printf("Pitch = %.1f Hz\n", pitch); 
-            
-            if(pitch == -0.5){
-              pitch = 0;
+      if (bufferIndex >= PITCH_BUFFER_SIZE) {
+        float avgPitch = calculateAveragePitch();
+        
+        bufferIndex = 0;
+
+        char pitchStr[10]; 
+        snprintf(pitchStr, sizeof(pitchStr), "%.1f", avgPitch);  
+
+        NimBLEService* pSvc = pServer->getServiceByUUID("BAAD");
+        if(pSvc) {
+          NimBLECharacteristic* pChr = pSvc->getCharacteristic("F00D");
+            if(pChr) {
+              Serial.printf("Pitch = %.1f Hz\n", avgPitch); 
+              pChr->setValue(pitchStr);
+              pChr->notify();
+              delay(20);
             }
-
-           
-    
         }
-    
-    delay(2);
+      }
+    }
+  }
+  delay(2);
 }
 
-
-static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle)
-{
-    adc_cali_handle_t handle = NULL;
-    esp_err_t ret = ESP_FAIL;
-    bool calibrated = false;
-
-#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
-    if (!calibrated) {
-        ESP_LOGI(TAG, "calibration scheme version is %s", "Curve Fitting");
-        adc_cali_curve_fitting_config_t cali_config = {
-            .unit_id = unit,
-            .chan = channel,
-            .atten = atten,
-            .bitwidth = ADC_BITWIDTH_DEFAULT,
-        };
-        ret = adc_cali_create_scheme_curve_fitting(&cali_config, &handle);
-        if (ret == ESP_OK) {
-            calibrated = true;
-        }
-    }
-#endif
-
-#if ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
-    if (!calibrated) {
-        ESP_LOGI(TAG, "calibration scheme version is %s", "Line Fitting");
-        adc_cali_line_fitting_config_t cali_config = {
-            .unit_id = unit,
-            .atten = atten,
-            .bitwidth = ADC_BITWIDTH_DEFAULT,
-        };
-        ret = adc_cali_create_scheme_line_fitting(&cali_config, &handle);
-        if (ret == ESP_OK) {
-            calibrated = true;
-        }
-    }
-#endif
-
-    *out_handle = handle;
-    if (ret == ESP_OK) {
-        ESP_LOGI(TAG, "Calibration Success");
-    } else if (ret == ESP_ERR_NOT_SUPPORTED || !calibrated) {
-        ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
-    } else {
-        ESP_LOGE(TAG, "Invalid arg or no memory");
-    }
-
-    return calibrated;
-}
-
-static void example_adc_calibration_deinit(adc_cali_handle_t handle)
-{
-#if ADC_CALI_SCHEME_CURVE_FITTING_SUPPORTED
-    ESP_LOGI(TAG, "deregister %s calibration scheme", "Curve Fitting");
-    ESP_ERROR_CHECK(adc_cali_delete_scheme_curve_fitting(handle));
-
-#elif ADC_CALI_SCHEME_LINE_FITTING_SUPPORTED
-    ESP_LOGI(TAG, "deregister %s calibration scheme", "Line Fitting");
-    ESP_ERROR_CHECK(adc_cali_delete_scheme_line_fitting(handle));
-#endif
-}
